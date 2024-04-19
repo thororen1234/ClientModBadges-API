cca1f384497fbfc717eb11f21923f44c1381f4e8
nyx <60797172+verticalsync@users.noreply.github.com>
2024-04-19 03:34:22 +0300

    update vencord badges to new api

--- scripts/getVencordBadges.mjs
+++ scripts/getVencordBadges.mjs
@@ -1,34 +1,54 @@
 import axios from "axios";
 import * as utils from "./utils.mjs";
+
 const { addUser, CLIENT_MODS } = utils;
+
 let attempts = 1;
 
 const getVencordBadges = async () => {
     try {
-        const { data } = await axios.get("https://raw.githubusercontent.com/Vendicated/Vencord/main/src/utils/constants.ts", { headers: { "Cache-Control": "no-cache" } });
+        const { data } = await axios.get(
+            "https://raw.githubusercontent.com/Vendicated/Vencord/main/src/utils/constants.ts",
+            { headers: { "Cache-Control": "no-cache" } }
+        );
+
         const matches = data.match(/id: ([0-9n]+)/gs);
         const contributors = matches.map(match => {
             const [, id] = match.match(/id: ([0-9n]+)/s);
             return { id: id.replace("n", ""), badges: ["Contributor"] };
         });
-        const { data: donorData } = await axios.get("https://gist.githubusercontent.com/Vendicated/51a3dd775f6920429ec6e9b735ca7f01/raw/badges.csv", { headers: { "Cache-Control": "no-cache" } });
-        const donors = donorData.split("\n").map((line, i) => {
-            if (i === 0) return;
-            const [id, name, badge] = line.split(",");
-            return { id, badges: [{ name, badge }] };
-        }).filter(d => d);
+
+        const { data: donorData } = await axios.get(
+            "https://badges.vencord.dev/badges.json",
+            { headers: { "Cache-Control": "no-cache" } }
+        );
+
+        const donors = Object.entries(donorData).map(([id, badge]) => {
+            const badgesArray = Object.entries(badge).map(([name, value]) => ({
+                name: value.tooltip,
+                badge: value.badge,
+            }));
+
+            return {
+                id,
+                badges: badgesArray,
+            };
+        });
+
         let users = [...contributors, ...donors];
+
         users = users.reduce((acc, user) => {
             const existingUser = acc.find(u => u.id === user.id);
-            if (existingUser) existingUser.badges = [...existingUser.badges, ...user.badges];
+            if (existingUser)
+                existingUser.badges = [...existingUser.badges, ...user.badges];
             else acc.push(user);
             return acc;
         }, []);
+
         users.forEach(user => addUser(user.id, CLIENT_MODS.VENCORD, user.badges));
     } catch (e) {
-        if (attempts++ > 4) console.error("Failed to get Vencord badges after 5 attempts", e);
+        if (attempts++ > 4)
+            console.error("Failed to get Vencord badges after 5 attempts", e);
         else setTimeout(getVencordBadges, 500);
     }
 };
-
-getVencordBadges();
